a2080e7e83871f994a6f8ff6ecc2c896198d08b2
YALLICO OWNER <yallico2024@gmail.com>
2026-07-09 22:22:59 -0500

    Actualizar group-delete.js

--- plugins/group-delete.js
+++ plugins/group-delete.js
@@ -1,13 +1,14 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
 
-if (!m.quoted) return conn.reply(m.chat, `*${emojis} Responde al mensaje que deseas eliminar.*`, m)
+if (!m.quoted) return conn.reply(m.chat, `⛈️ *RAYO PREM DELETE* 🌙\n\n⚡ Responde al mensaje que deseas eliminar.`, m) // Cambiado
 try {
 let delet = m.message.extendedTextMessage.contextInfo.participant
 let bang = m.message.extendedTextMessage.contextInfo.stanzaId
-return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+conn.reply(m.chat, `🌩️ *RAYO PREM* ➔ Mensaje eliminado\n⚡ *Acción por:* @${m.sender.split('@')[0]}\n🌙 *Team Nightwish*`, m, { mentions: [m.sender] }) // Cambiado
  } catch {
-return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
-}
+await conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
+conn.reply(m.chat, `🌩️ *RAYO PREM* ➔ Mensaje eliminado\n⚡ *Acción por:* @${m.sender.split('@')[0]}\n🌙 *Team Nightwish*`, m, { mentions: [m.sender] }) // Cambiado
 }
 handler.help = ['delete']
 handler.tags = ['grupos']
