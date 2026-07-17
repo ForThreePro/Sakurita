2c388ee73bab4d0a09325cbd13a53f2821b81dd7
YALLICO OWNER <yallico2024@gmail.com>
2026-07-17 13:00:09 -0500

    Actualizar group-link.js

--- plugins/group-link.js
+++ plugins/group-link.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
     try {
         let link = await conn.groupInviteCode(m.chat)
-        let text = `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+        let text = `╭─❒ *『 𝗖𝗬𝗕𝗘𝗥 𝗕𝗢𝗧 』* ❒
 │ 🔗 *LINK DEL GRUPO*
 │
 │ ⚡ https://chat.whatsapp.com/${link}
@@ -10,11 +10,11 @@ let handler = async (m, { conn }) => {
 ╰─────────────────❒`
         m.reply(text)
     } catch {
-        m.reply(`╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
-│ ⛈️ *ERROR*
+        m.reply(`╭─❒ *『 𝗖𝗬𝗕𝗘𝗥 𝗕𝗢𝗧 』* ❒
+│ ❌ *ERROR DE SISTEMA*
 │
 │ ⚡ *No pude obtener el link*
-│ 🌙 *¿Soy administrador del grupo?*
+│ 🤖 *¿Soy administrador del grupo?*
 ╰─────────────────❒`)
     }
 }
