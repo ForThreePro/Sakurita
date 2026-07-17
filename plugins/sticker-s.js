35cd57994d3562fea6599c5d7ca26cfe1a733038
YALLICO OWNER <yallico2024@gmail.com>
2026-07-17 13:21:56 -0500

    Actualizar sticker-s.js

--- plugins/sticker-s.js
+++ plugins/sticker-s.js
@@ -3,20 +3,20 @@ import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || q.mediaType || ''
-    if (!/webp|image|video/g.test(mime)) return m.reply(`╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+    if (!/webp|image|video/g.test(mime)) return m.reply(`╭─❒ *『 𝗖𝗬𝗕𝗘𝗥 𝗕𝗢𝗧 』* ❒
 │ 🎭 *STICKER*
 │
 │ ⚠️ *Responde a una imagen, video o gif*
-│ 🌙 *Para convertirlo en sticker*
+│ 💻 *Para convertirlo en sticker*
 ╰─────────────────❒`)
-    
+
     let img = await q.download()
-    let stiker = await sticker(img, false, 'Team Nightwish', 'Whois Yallico')
-    
-    await conn.sendFile(m.chat, stiker, 'sticker.webp', `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+    let stiker = await sticker(img, false, 'Cyber Bot', 'Whois Yallico')
+
+    await conn.sendFile(m.chat, stiker, 'sticker.webp', `╭─❒ *『 𝗖𝗬𝗕𝗘𝗥 𝗕𝗢𝗧 』* ❒
 │ ⚡ *STICKER CREADO*
 │
-│ > *El trueno en forma de sticker*
+│ > *El sistema en forma de sticker*
 ╰─────────────────❒`, m)
 }
 
