9dd59673a77edcb022cda6ff9d7bef1238f3fe49
YALLICO OWNER <yallico2024@gmail.com>
2026-07-19 15:39:15 -0500

    Actualizar group-top.js

--- plugins/group-top.js
+++ plugins/group-top.js
@@ -4,11 +4,11 @@ import path from 'path'
 let user = a => '@' + a.split('@')[0]
 
 function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
-if (!text) return m.reply(`╭─❒ *『 𝗖𝗬𝗕𝗘𝗥 𝗕𝗢𝗧 』* ❒
-│ 🏆 *TOP 10*
+if (!text) return m.reply(`🌸╭─── SAKURITA BOT ───╮🌸
+│ 🌷 *TOP 10*
 │
-│ ⚡ *Ejemplo:*.top gente mas activa
-╰─────────────────❒`)
+│ 🌸 *Ejemplo:*.top pétalos más activos
+╰───────────────────────╯`)
 
 let ps = groupMetadata.participants.map(v => v.id)
 let a = ps.getRandom()
@@ -22,25 +22,25 @@ let h = ps.getRandom()
 let i = ps.getRandom()
 let j = ps.getRandom()
 let k = Math.floor(Math.random() * 70);
-let x = `${pickRandom(['✅','📊','💻','🤖','🔥','💀','😎','🤙','👑','💥'])}`
+let x = `${pickRandom(['🌸','🌷','🌺','🍂','🌼','🥀','🌹','🌻','🌵','🌱'])}`
 let vn = `https://hansxd.nasihosting.com/sound/sound${k}.mp3`
 
-let top = `╭─❒ *『 𝗖𝗬𝗕𝗘𝗥 𝗕𝗢𝗧 』* ❒
-│ 🏆 *TOP 10: ${text.toUpperCase()}*
+let top = `🌸╭─── SAKURITA BOT ───╮🌸
+│ 🌷 *TOP 10: ${text.toUpperCase()}*
 │
 │ 👑 *1.* ${user(a)}
 │ 👑 *2.* ${user(b)}
 │ 👑 *3.* ${user(c)}
-│ 💻 *4.* ${user(d)}
-│ 💻 *5.* ${user(e)}
-│ 💻 *6.* ${user(f)}
-│ 💻 *7.* ${user(g)}
-│ 💻 *8.* ${user(h)}
-│ 💻 *9.* ${user(i)}
-│ 💻 *10.* ${user(j)}
+│ 🌸 *4.* ${user(d)}
+│ 🌸 *5.* ${user(e)}
+│ 🌸 *6.* ${user(f)}
+│ 🌸 *7.* ${user(g)}
+│ 🌸 *8.* ${user(h)}
+│ 🌸 *9.* ${user(i)}
+│ 🌸 *10.* ${user(j)}
 │
-│ > *“El sistema elige a los mejores”*
-╰─────────────────❒`
+│ > *“El jardín florece con los mejores pétalos”*
+╰───────────────────────╯`
 
 m.reply(top, null, { mentions: [a, b, c, d, e, f, g, h, i, j]})
 }
